67ccb414bde47b1ec5648e02ecd568bf8e9cc895
LexPrimen <105244473+LexPrimen@users.noreply.github.com>
2022-08-30 14:54:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ const nodemailer = require('nodemailer');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// const conexion = mysql.createConnection({
-//     host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE   
-// });
+const conexion = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE   
+});
 
 
-// console.log();
+console.log();
 
 
-//    conexion.connect((err) => {
-//     if (err) {
-//         console.error(`error de conexion: ${err.stack}`)
-//         return;
-//     }
-//     console.log(`conectado a la base de datos ${process.env.DATABASE}`);
-// });
+   conexion.connect((err) => {
+    if (err) {
+        console.error(`error de conexion: ${err.stack}`)
+        return;
+    }
+    console.log(`conectado a la base de datos ${process.env.DATABASE}`);
+});
 
 
 
@@ -74,83 +74,83 @@ app.get('/registro', (req,res) =>{
 });
 
 
-// app.post('/registro', (req,res) => {
+app.post('/registro', (req,res) => {
     
    
-    //  const {nombre,apellido,edad,mail,usuario,contraseña} = req.body; 
+     const {nombre,apellido,edad,mail,usuario,contraseña} = req.body; 
         
-    //     console.log(nombre,apellido,edad,mail,usuario,contraseña);
+        console.log(nombre,apellido,edad,mail,usuario,contraseña);
      
-    //         if (nombre =='' || apellido == '' || edad =='' ||  usuario =='' || contraseña =='' ){
-    //         let validacion = 'Rellenar los campos';
+            if (nombre =='' || apellido == '' || edad =='' ||  usuario =='' || contraseña =='' ){
+            let validacion = 'Rellenar los campos';
     
-    //         res.render('registro', {
-    //             titulo: 'Registrarse',
-    //             style:'styles.css',
-    //             validacion
-    //         });
-    //        } else{
-    //         res.render('validacion', {
-    //             titulo: 'Registro con Exito ',
-    //             style:'styles.css',
-    //       }); 
+            res.render('registro', {
+                titulo: 'Registrarse',
+                style:'styles.css',
+                validacion
+            });
+           } else{
+            res.render('validacion', {
+                titulo: 'Registro con Exito ',
+                style:'styles.css',
+          }); 
         
-    //      }
-    //      if (nombre == '' || mail == ''){
-    //         let validacion = 'rellenar los campos necesarios..';
+         }
+         if (nombre == '' || mail == ''){
+            let validacion = 'rellenar los campos necesarios..';
                  
             
-    //      } else{
+         } else{
  
-    //      console.log(nombre),
-    //      console.log(mail);
+         console.log(nombre),
+         console.log(mail);
  
-    //       async function envioMail(){
+          async function envioMail(){
  
-    //        let transporter = nodemailer.createTransport({
-    //            host: 'smtp.gmail.com',
-    //            port: 465,
-    //            secure: true,
-    //            auth: {
-    //                user: 'Bonolucas1965@gmail.com',
-    //                pass: 'fdysivcfbgvtzotd',
-    //            }
-    //        });
+           let transporter = nodemailer.createTransport({
+               host: 'smtp.gmail.com',
+               port: 465,
+               secure: true,
+               auth: {
+                   user: 'xxxxxx@gmai.com',
+                   pass: 'xxxxxxxxxxxxxxxxx',
+               }
+           });
             
-    //        let envio = await transporter.sendMail({
-    //            from: process.env.USERMAIL,
-    //            to: `${mail}`,
-    //            subject: 'Gracias por Suscribirse a nuestra pagina web',
-    //            html: `Dudas y consultas envie un mail <br>
-    //            Saludos `
-    //        });
+           let envio = await transporter.sendMail({
+               from: process.env.USERMAIL,
+               to: `${mail}`,
+               subject: 'Gracias por Suscribirse a nuestra pagina web',
+               html: `Dudas y consultas envie un mail <br>
+               Saludos `
+           });
             
      
            
-    //    } 
+       } 
  
-    //    envioMail();
+       envioMail();
        
     
-    //     let datos = {
-    //         Nombre: nombre,     
-    //         Apellido: apellido,
-    //         Edad: edad,
-    //         Mail: mail,
-    //         Usuario: usuario,
-    //         Contraseña: contraseña
-    //     }
+        let datos = {
+            Nombre: nombre,     
+            Apellido: apellido,
+            Edad: edad,
+            Mail: mail,
+            Usuario: usuario,
+            Contraseña: contraseña
+        }
          
-    //       let sql ='INSERT INTO users set ?';
+          let sql ='INSERT INTO users set ?';
     
-    //       conexion.query(sql, datos, (err, result) => {
-    //         if (err) throw err;
+          conexion.query(sql, datos, (err, result) => {
+            if (err) throw err;
              
-    //         return
+            return
                  
-    //          });
-    //         }
-    //     });
+             });
+            }
+        });
           
           
         
@@ -219,5 +219,5 @@ app.get('/registro', (req,res) =>{
 
 
 app.listen(PORT, () => {
-    // console.log(`el servidor esta funcionando en el puerto ${PORT}`)
+    console.log(`el servidor esta funcionando en el puerto ${PORT}`)
  });
